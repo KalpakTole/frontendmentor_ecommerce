e249d7682bc9db061207079dc8375e96adb87665
Kalpak <kalpakto26@gmail.com>
2022-01-28 02:01:42 +0530

    Button initial styling completed

--- src/components/ProductDescription.js
+++ src/components/ProductDescription.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import classes from './ProductDescription.module.css';
+import CartIcon from '../assets/icon-cart.svg';
+import MinusIcon from '../assets/icon-minus.svg';
+import PlusIcon from '../assets/icon-plus.svg';
 
 const ProductDescription = () => {
+	const [curAmount, setCurAmount] = useState(0);
+	const [showDecrease, setShowDecrease] = useState(false);
+
+	const amountHandler = (type) => {
+		if (type === 'decrease') {
+			setCurAmount((prevState) => prevState - 1);
+		} else if (type === 'increase') {
+			setCurAmount((prevState) => prevState + 1);
+		}
+	};
+
+	const addToCartHandler = () => {};
+
+	useEffect(() => {
+		if (curAmount <= 0) {
+			setShowDecrease(false);
+		} else {
+			setShowDecrease(true);
+		}
+	}, [curAmount]);
+
 	return (
 		<div className={classes['product-description']}>
 			<div className={classes['description-wrapper']}>
@@ -16,6 +40,25 @@ const ProductDescription = () => {
 					<span className={classes.discount}>50%</span>
 				</div>
 				<p className={classes['original-price']}>$250.00</p>
+				<div className={classes['button-container']}>
+					<span className={classes['change-amount']}>
+						<button
+							className={classes['minus-button']}
+							disabled={!showDecrease}
+							onClick={() => amountHandler('decrease')}
+						>
+							<img src={MinusIcon} alt='Decrease quantity to be added' />
+						</button>
+						<span className={classes['current-amount']}>{curAmount}</span>
+						<button className={classes['plus-button']} onClick={() => amountHandler('increase')}>
+							<img src={PlusIcon} alt='Increase quantity to be added' />
+						</button>
+					</span>
+					<button className={classes['add-to-cart']} onClick={addToCartHandler}>
+						<img src={CartIcon} alt='Add to cart Icon' className={classes['add-to-cart-icon']} />
+						Add to cart
+					</button>
+				</div>
 			</div>
 		</div>
 	);
